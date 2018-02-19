c49ccd45136c3280ae1ce87b934799377bc06ddf
Stephen Eyerly <seyerly@seas.upenn.edu>
2018-02-19 00:19:04 -0500

    added in login activity with Dynamo Db import

--- app/src/main/java/lifestats/a350s18_21_lifestats/MainActivity.java
+++ app/src/main/java/lifestats/a350s18_21_lifestats/MainActivity.java
@@ -2,12 +2,15 @@ package lifestats.a350s18_21_lifestats;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.amazonaws.mobile.client.AWSMobileClient;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        AWSMobileClient.getInstance().initialize(this).execute();
         setContentView(R.layout.activity_main);
     }
 }
