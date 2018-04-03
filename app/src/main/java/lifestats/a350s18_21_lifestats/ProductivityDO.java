cebe962b5216cb153d18592358b247be7e024585
Stephen Eyerly <seyerly@seas.upenn.edu>
2018-04-03 00:02:52 -0400

    added in DO files

---
+++ app/src/main/java/lifestats/a350s18_21_lifestats/ProductivityDO.java
@@ -0,0 +1,38 @@
+package com.amazonaws.models.nosql;
+
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBAttribute;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBHashKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBIndexHashKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBIndexRangeKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBRangeKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBTable;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@DynamoDBTable(tableName = "lifestats-mobilehub-2078005233-productivity")
+
+public class ProductivityDO {
+    private String _userId;
+    private Set<String> _productivity;
+
+    @DynamoDBHashKey(attributeName = "userId")
+    @DynamoDBAttribute(attributeName = "userId")
+    public String getUserId() {
+        return _userId;
+    }
+
+    public void setUserId(final String _userId) {
+        this._userId = _userId;
+    }
+    @DynamoDBAttribute(attributeName = "productivity")
+    public Set<String> getProductivity() {
+        return _productivity;
+    }
+
+    public void setProductivity(final Set<String> _productivity) {
+        this._productivity = _productivity;
+    }
+
+}
---
+++ app/src/main/java/lifestats/a350s18_21_lifestats/StressDO.java
@@ -0,0 +1,38 @@
+package com.amazonaws.models.nosql;
+
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBAttribute;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBHashKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBIndexHashKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBIndexRangeKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBRangeKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBTable;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@DynamoDBTable(tableName = "lifestats-mobilehub-2078005233-stress")
+
+public class StressDO {
+    private String _userId;
+    private Set<String> _stress;
+
+    @DynamoDBHashKey(attributeName = "userId")
+    @DynamoDBAttribute(attributeName = "userId")
+    public String getUserId() {
+        return _userId;
+    }
+
+    public void setUserId(final String _userId) {
+        this._userId = _userId;
+    }
+    @DynamoDBAttribute(attributeName = "stress")
+    public Set<String> getStress() {
+        return _stress;
+    }
+
+    public void setStress(final Set<String> _stress) {
+        this._stress = _stress;
+    }
+
+}
