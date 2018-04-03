3e5855480dfd92b7cee0989d35a1ae85bc8e39a7
Stephen Eyerly <seyerly@seas.upenn.edu>
2018-04-02 22:54:01 -0400

    removed test data from contstructor

--- app/src/main/java/lifestats/a350s18_21_lifestats/MoneySpent.java
+++ app/src/main/java/lifestats/a350s18_21_lifestats/MoneySpent.java
@@ -31,13 +31,7 @@ public class MoneySpent extends AppCompatActivity {
         Button latestSpending = findViewById(R.id.latestSpending);
         Button moneyGraph = findViewById(R.id.moneyGraph);
         final EditText moneyText = findViewById(R.id.moneyText);
-
-
-        moneyPerDay.put("01/31/1997", 2.50);
-        moneyPerDay.put("01/31/1998", 3.50);
-        moneyPerDay.put("11/13/2000", 9.50);
-        moneyPerDay.put("08/22/1998", 7.50);
-        moneyPerDay.put("08/31/1998", 32.50);
+        
 
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
