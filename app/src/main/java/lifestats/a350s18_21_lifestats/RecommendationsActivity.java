8ec3d5921195e341dc6f3088429b74c1fc947b49
Kevin <simkevin97@gmail.com>
2018-04-20 01:42:20 -0400

    made recommendation classes

--- app/src/main/java/lifestats/a350s18_21_lifestats/RecommendationsActivity.java
+++ app/src/main/java/lifestats/a350s18_21_lifestats/RecommendationsActivity.java
@@ -1,7 +1,9 @@
 package lifestats.a350s18_21_lifestats;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class RecommendationsActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class RecommendationsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommendations);
+    }
+
+    public void openStress(View view) {
+        Intent intent = new Intent(this, StressRecommendationsActivity.class);
+        startActivity(intent);
+    }
+
+    public void openHappiness(View view) {
+        Intent intent = new Intent(this, HappinessRecommendationsActivity.class);
+        startActivity(intent);
+    }
+
+    public void openProductivity(View view) {
+        Intent intent = new Intent(this, ProductivityRecommendationsActivity.class);
+        startActivity(intent);
     }
 }
