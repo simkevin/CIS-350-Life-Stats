c1bf8a366b2ceb7db7aab1dc974f383840abc3cd
Stephen Eyerly <seyerly@seas.upenn.edu>
2018-04-20 22:31:53 -0400

    added in Aren's updates

--- app/src/main/java/lifestats/a350s18_21_lifestats/QuantitativeDataEntry.java
+++ app/src/main/java/lifestats/a350s18_21_lifestats/QuantitativeDataEntry.java
@@ -1,5 +1,6 @@
 package lifestats.a350s18_21_lifestats;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -10,7 +11,7 @@ import android.widget.Toast;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-// this activity is where happiness, productivity, and stress data is inputed and stored
+// this activity is where happiness, productivity, and stress data is inputted and stored
 public class QuantitativeDataEntry extends AppCompatActivity {
 
     private static HappinessWrapper storeHappiness = HappinessWrapper.getInstance();
@@ -26,6 +27,7 @@ public class QuantitativeDataEntry extends AppCompatActivity {
         final RatingBar productivityBar = (RatingBar) findViewById(R.id.productivityBar);
         final RatingBar stressBar = (RatingBar) findViewById(R.id.stressBar);
         Button submitButton = (Button) findViewById(R.id.submitButton);
+        Button setHappinessButton = (Button) findViewById(R.id.setHappiness);
 
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,19 +39,37 @@ public class QuantitativeDataEntry extends AppCompatActivity {
                                 "Happiness Rating: " + happinessBar.getRating() + "\n" +
                                 "Productivity Rating: " + productivityBar.getRating() + "\n" +
                                 "Stress Rating: " + stressBar.getRating()
-                        , Toast.LENGTH_LONG).show();
+                        , Toast.LENGTH_SHORT).show();
 
                 storeHappiness.put(date, happinessBar.getRating());
                 storeProductivity.put(date, productivityBar.getRating());
                 storeStress.put(date, stressBar.getRating());
 
+                double globalAverage = storeHappiness.averageAll();
+
                 // reset all bars
                 happinessBar.setRating(0F);
                 productivityBar.setRating(0F);
                 stressBar.setRating(0F);
+
+                if (globalAverage < SetHappiness.happinessLowerBound) {
+                    Toast.makeText(getApplicationContext()
+                            , "Warning: Happiness level is too low. Your min threshold is " +
+                                    SetHappiness.happinessLowerBound + " your average is " + globalAverage
+                            , Toast.LENGTH_LONG).show();
+                }
             }
 
         });
+
+        setHappinessButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(QuantitativeDataEntry.this, SetHappiness.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 
